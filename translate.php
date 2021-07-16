5df785e491695f9d5b3b6fa00498f63060555aa8
Kamil <kamil.odo@iware.pl>
2021-07-16 13:06:19 +0200

    Initial commit

---
+++ translate.php
@@ -0,0 +1,36 @@
+<?php
+
+function parseItem(&$data, $item)
+{
+    foreach ($item as $translationKey => $translationValue) {
+        if (is_array($translationValue)) {
+            if (empty($data[$translationKey])) {
+                $data[$translationKey] = [];
+            }
+
+            parseItem($data[$translationKey], $translationValue);
+            continue;
+        }
+
+
+        if (empty($data[$translationKey])) {
+            $data[$translationKey] = "MISSING_TRANSLATION($translationValue)";
+        }
+    }
+}
+
+$data = file_get_contents('translations.json');
+$decoded = json_decode($data, true);
+echo PHP_EOL;
+foreach ($decoded['en'] as $key => $item) {
+    if (empty($decoded['pl'][$key])) {
+        $decoded['pl'][$key] = [];
+    }
+
+    parseItem($decoded['pl'][$key], $item);
+}
+
+$encoded = json_encode($decoded, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+file_put_contents('translations-fixed.json', $encoded);
+echo 'Generowanie zakończone';
+
